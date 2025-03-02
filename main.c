bfe5809bfa9c2a5b8d343059a3177b23a44d821d
cdrmack <git.pynvt@passmail.net>
2025-03-02 14:40:04 +0100

    add example with input and output to/from thread

--- main.c
+++ main.c
@@ -23,14 +23,35 @@ void bar()
     }
 }
 
+void *add_magic(void *arg)
+{
+    int *base = (int *)arg;
+    int *result = (int *)malloc(sizeof(int));
+
+    printf("[add] arg = %d\n", *base);
+
+    *result = *base + 42;
+    printf("[add] result = %d\n", *result);
+    return result;
+}
+
 int main()
 {
-    pthread_t foo_thread;
-    pthread_create(&foo_thread, NULL, foo, NULL);
+    /* pthread_t foo_thread; */
+    /* pthread_create(&foo_thread, NULL, foo, NULL); */
 
-    bar();
+    /* bar(); */
 
-    pthread_join(foo_thread, NULL);
+    /* pthread_join(foo_thread, NULL); */
+
+    int in = 8;
+    pthread_t add_thread;
+    pthread_create(&add_thread, NULL, add_magic, &in);
+
+    int *out;
+    pthread_join(add_thread, (void *)&out);
+
+    printf("[main] result = %d\n", *out);
 
     return EXIT_SUCCESS;
 }
